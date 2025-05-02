95a227e2f0950f2c72634674e7f2c59eae4f5b2a
otoplus-example <mahmutenescetin@gmail.com>
2025-05-02 17:12:24 +0300

    feat: base view model enhanced with flow and state management

--- lib/core/base/base_viewmodel.dart
+++ lib/core/base/base_viewmodel.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
 
 abstract class BaseViewModel extends ChangeNotifier {
   bool _isLoading = false;
   bool _isError = false;
   String? _errorMessage;
+  bool _isDisposed = false;
+  bool _isBusy = false;
+  int _flowCount = 0;
 
   bool get isLoading => _isLoading;
   bool get isError => _isError;
   String? get errorMessage => _errorMessage;
+  bool get isDisposed => _isDisposed;
+  bool get isBusy => _isBusy;
 
   void setLoading(bool value) {
     _isLoading = value;
@@ -26,4 +32,52 @@ abstract class BaseViewModel extends ChangeNotifier {
     notifyListeners();
   }
 
+  void notify() {
+    if (!_isDisposed) {
+      notifyListeners();
+    }
+  }
+
+  Future<void> flow<T extends Object?>(
+    final Function callback, {
+    final ValueChanged<T>? onSuccess,
+    final void Function(StackTrace, DioException)? onError,
+    final void Function()? onFinally,
+    final bool showLoading = true,
+    final bool shouldWaitOtherFlows = false,
+  }) async {
+    _flowCount++;
+    setLoading(showLoading);
+    _isBusy = true;
+    notify();
+
+    try {
+      final T data = await callback();
+      onSuccess?.call(data);
+    } on DioException catch (dioException, stackTrace) {
+      setError(true, message: dioException.message);
+      onError?.call(stackTrace, dioException);
+    } catch (e) {
+      setError(true, message: e.toString());
+    } finally {
+      onFinally?.call();
+
+      _flowCount--;
+      if (!shouldWaitOtherFlows) {
+        setLoading(false);
+      } else {
+        if (_flowCount == 0) {
+          setLoading(false);
+        }
+      }
+      _isBusy = false;
+      notify();
+    }
+  }
+
+  @override
+  void dispose() {
+    _isDisposed = true;
+    super.dispose();
+  }
 }
